03f9cc1f0f0c2e1a46cac66ffe215169a14334a6
Suresh Kumar <34769206+sureshk2a@users.noreply.github.com>
2021-07-08 10:29:57 +0530

    initial commit

---
+++ NvidiaGPUStockUpdate_Bot/main.py
@@ -0,0 +1,17 @@
+import json
+import telebot
+
+f = open('keys.json',)
+API_KEY = json.load(f)['NvidiaBot']
+bot = telebot.TeleBot(API_KEY)
+
+@bot.message_handler(commands=['Greet'])
+def greet(message):
+  bot.reply_to(message, "Hey! Hows it going?")
+
+@bot.message_handler(commands=['hello'])
+def hello(message):
+  bot.send_message(message.chat.id, "Hello!")
+
+
+bot.polling()
